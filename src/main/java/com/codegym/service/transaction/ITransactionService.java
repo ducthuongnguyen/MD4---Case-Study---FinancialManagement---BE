543231a792b6bf97cca7e5ba57d2411ecb0e56ea
vanh2001 <anhviet1709@gmail.com>
2022-06-24 09:16:31 +0700

    Add file find Transaction

--- src/main/java/com/codegym/service/transaction/ITransactionService.java
+++ src/main/java/com/codegym/service/transaction/ITransactionService.java
@@ -3,5 +3,8 @@ package com.codegym.service.transaction;
 import com.codegym.model.Transaction;
 import com.codegym.service.IGeneralService;
 
+import java.time.LocalDateTime;
+
 public interface ITransactionService extends IGeneralService<Transaction> {
+    Iterable<Transaction> findAllByCreatedDateBetween(LocalDateTime fromTime, LocalDateTime toTime);
 }
